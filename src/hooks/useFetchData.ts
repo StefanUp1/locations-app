9e4a49ae491da46d06c988abffbb4557a6dfae69
Stefan Ljubinkovic <sljubinkovic@apple.com>
2021-12-12 19:35:04 +0100

    Create "useFetchData" custom hook.

---
+++ src/hooks/useFetchData.ts
@@ -0,0 +1,36 @@
+import { useState, useEffect, useRef } from "react";
+
+const useFetchData = <T>(fetchMethod: () => Promise<T>) => {
+  const [data, setData] = useState<T>();
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const mounted = useRef(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsError(false);
+      setIsLoading(true);
+
+      try {
+        const result = await fetchMethod();
+        // This prevents errors from trying to set state on unmounted component
+        if (!mounted.current) return;
+        setData(result);
+      } catch (error) {
+        setIsError(true);
+      }
+
+      setIsLoading(false);
+    };
+
+    fetchData();
+
+    return () => {
+      mounted.current = false;
+    };
+  }, [fetchMethod]);
+
+  return { data, isLoading, isError };
+};
+
+export default useFetchData;
