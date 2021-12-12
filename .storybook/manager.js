6f29d9d951bebfc0c2f0da9025d7eafdd3a4bb30
Stefan Ljubinkovic <sljubinkovic@apple.com>
2021-12-12 01:57:23 +0100

    Switch to Storybook "light" theme.

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -1,9 +1,9 @@
-import { addons } from '@storybook/addons'
-import { create, themes } from '@storybook/theming'
+import { addons } from "@storybook/addons";
+import { create, themes } from "@storybook/theming";
 
 addons.setConfig({
-    theme: create({
-        base: 'dark',
-        brandTitle: 'ACME',
-    })
-})
+  theme: create({
+    base: "light",
+    brandTitle: "ACME",
+  }),
+});
