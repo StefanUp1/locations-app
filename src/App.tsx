ee67edf3ff6d8e301d8210b8859fc9f4aca6120f
Stefan Ljubinkovic <sljubinkovic@apple.com>
2021-12-12 19:33:19 +0100

    Create "Loader" component and use it.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React, { Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
+import Loader from "components/Loader";
 /** Lazy loading routes */
 const AllLocations = React.lazy(() => import("pages/AllLocations"));
 const NotFound = React.lazy(() => import("pages/NotFound"));
 
 function App() {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
+    <Suspense fallback={<Loader />}>
       <Routes>
         <Route path="/" element={<AllLocations />} />
         <Route path="*" element={<NotFound />} />
---
+++ src/components/Loader/Loader.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./loader.scss";
+
+// Here we could create "Loader" component to display while waiting for api call to finish, component to lazy load, etc...
+const Loader = () => <div className="loader">Loading....</div>;
+
+export default Loader;
