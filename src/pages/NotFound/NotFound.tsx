07da69d5d0c2a52efd2f2742a7d95b9beac67a42
Stefan Ljubinkovic <sljubinkovic@apple.com>
2021-12-12 22:41:22 +0100

    Style "Not Found" page.

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import "./not-found.scss";
+import { Link } from "react-router-dom";
 
 const NotFound = (): JSX.Element => {
-  return <div>404</div>;
+  return (
+    <div className="not-found">
+      <p>404</p>
+      <Link to="/">Back to Homepage</Link>
+    </div>
+  );
 };
 
 export default NotFound;
